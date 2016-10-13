dfa8c0be46cc6ce73d6a014c1678e6fad2e986ad
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-13 08:51:23 +0300

    Проведена оптимизация кода
    Функции get_file_ information и get_equal_files объединены в одну функцию get_equal files. В результате, не нужно предвычислять кортежи информации о каждом файле и вместо двух проходов по списку файлов для решения задачи достаточно одного.

--- duplicates.py
+++ duplicates.py
@@ -12,24 +12,18 @@ def get_filepath(dir_address, filename):
     return join(dir_address, filename)
 
 
-def get_files_information(directory_path):
-    files_information = []
-    for dir_address, dirs, files in walk(directory_path):
-        for file in files:
-            filepath = get_filepath(dir_address, file)
-            files_information.append([file, get_file_size(filepath), filepath])
-    return files_information
-
-
-def get_equal_files(files_information):
+def get_equal_files(directory_path):
     equal_files = []
     files_dictionary = {}
-    for filename, filesize, filepath in files_information:
-        if files_dictionary.get(filename) is not None:
-            if files_dictionary[filename][0] == filesize:
-                equal_files.append([filepath, files_dictionary[filename][1]])
-        else:
-            files_dictionary[filename] = [filesize, filepath]
+    for dir_address, dirs, files in walk(directory_path):
+        for filename in files:
+            filepath = get_filepath(dir_address, filename)
+            filesize = get_file_size(filepath)
+            if files_dictionary.get(filename) is not None:
+                if files_dictionary[filename][0] == filesize:
+                    equal_files.append([filepath, files_dictionary[filename][1]])
+            else:
+                files_dictionary[filename] = [filesize, filepath]
     return equal_files
 
 
@@ -42,8 +36,7 @@ def choose_directory_for_check():
 if __name__ == '__main__':
     dirpath = choose_directory_for_check()
     if dirpath:
-        files_information = get_files_information(dirpath)
-        equal_files = get_equal_files(files_information)
+        equal_files = get_equal_files(dirpath)
         print(u'Следующие файлы являются дубликатами:')
         pprint(equal_files)
     else:
