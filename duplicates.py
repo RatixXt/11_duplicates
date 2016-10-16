f7ac5025633017e9a21307beb83efb188cea5740
Shcherbakov Alexey <lovelessxt@gmail.com>
2016-10-16 19:29:12 +0300

    Проведена рефакторизация кода
    Убраны функции get_filepath и choose_directory_for_check.
    
    Изменено форматирование вывода в консоль.

--- duplicates.py
+++ duplicates.py
@@ -1,15 +1,10 @@
 # -*- coding: utf-8 -*-
 from os import walk, stat
 from os.path import join, exists
-from pprint import pprint
 
 
 def get_file_size(filepath):
     return stat(filepath).st_size
-
-
-def get_filepath(dir_address, filename):
-    return join(dir_address, filename)
 
 
 def get_equal_files(directory_path):
@@ -17,7 +12,7 @@ def get_equal_files(directory_path):
     files_dictionary = {}
     for dir_address, dirs, files in walk(directory_path):
         for filename in files:
-            filepath = get_filepath(dir_address, filename)
+            filepath = join(dir_address, filename)
             filesize = get_file_size(filepath)
             if files_dictionary.get(filename) is not None:
                 if files_dictionary[filename][0] == filesize:
@@ -27,17 +22,15 @@ def get_equal_files(directory_path):
     return equal_files
 
 
-def choose_directory_for_check():
-    dirpath = input(u'Введите путь к директории для проверки на дублирующиеся файлы:')
-    if exists(dirpath):
-        return dirpath
-
-
 if __name__ == '__main__':
-    dirpath = choose_directory_for_check()
+    dirpath = input(u'Введите путь к директории для проверки на дублирующиеся файлы:')
+    if not exists(dirpath):
+        dirpath = None
+
     if dirpath:
         equal_files = get_equal_files(dirpath)
         print(u'Следующие файлы являются дубликатами:')
-        pprint(equal_files)
+        for file in equal_files:
+            print('File1: {0[0]};\nFile2: {0[1]}\n'.format(file))
     else:
         print(u'Путь к директории указан неверно')
